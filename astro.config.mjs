daf189129924bd2826056661b28a81493a0a359e
lummin04-a11y <lummin04@gmail.com>
2025-11-19 01:55:15 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://lummin04-a11y.github.io',
   base: '/foodshaad-recetario',
+    integrations: [react()],
 });
